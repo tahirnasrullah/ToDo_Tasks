16775e2a41eeafa73285b126309ad3c20ac552e8
tahirnasrullah <tahirmehar151@gmail.com>
2026-02-03 02:24:14 +0500

    update   UI .0

--- lib/Pages/search_page.dart
+++ lib/Pages/search_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'NewHomePage/Elements.dart';
+
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
@@ -8,10 +10,19 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SearchField(searchController: searchController),
+          ],
+        ),
+      ),
     );
   }
 }
